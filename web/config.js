806959ff5093b56b60e6572c6f7aeef09092a226
elohimlima <elohimlima@localhost.localdomain>
2026-08-01 13:43:45 -0300

    chore: apontar pwa para web app publicada

--- web/config.js
+++ web/config.js
@@ -1,1 +1,1 @@
-window.APP_API_URL = 'https://script.google.com/macros/s/AKfycbynscVTVM4nQ0q4LUuFCVjW5lWDNouIvxNKZpENkOfHkpw-UOTBySLOlH8qvJyUkBEt/exec';
+window.APP_API_URL = 'https://script.google.com/macros/s/AKfycbwJLjvP_uuPspPgZReBLjGYHLPYLel8c-AgrD7SfUshYR-SfB3Tbp9AO5zLKDbEOQwfhQ/exec';
